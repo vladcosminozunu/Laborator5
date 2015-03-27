d2be75e7f4610d696dbb12f431f041a3d62b702b
student <vlad.ozunu@gmail.com>
2015-03-27 20:16:59 +0200

    implemented tasks for lab5

---
+++ labtasks/AddressBook/gen/ro/pub/cs/systems/pdsd/lab05/addressbook/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ro.pub.cs.systems.pdsd.lab05.addressbook;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- labtasks/AddressBook/src/ro/pub/cs/systems/pdsd/lab05/addressbook/controller/ContactAdapter.java
+++ labtasks/AddressBook/src/ro/pub/cs/systems/pdsd/lab05/addressbook/controller/ContactAdapter.java
@@ -1,15 +1,21 @@
 package ro.pub.cs.systems.pdsd.lab05.addressbook.controller;
 
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import ro.pub.cs.systems.pdsd.lab05.addressbook.R;
 import ro.pub.cs.systems.pdsd.lab05.addressbook.general.Constants;
+import ro.pub.cs.systems.pdsd.lab05.addressbook.general.Utilities;
 import ro.pub.cs.systems.pdsd.lab05.addressbook.model.Contact;
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract.Contacts;
@@ -75,34 +81,108 @@ public class ContactAdapter extends BaseAdapter implements LoaderManager.LoaderC
 
 	@Override
 	public int getCount() {
-		//TODO: exercise 5b
-		return -1;
+		return data.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
 		//TODO: exercise 5b
-		return null;
+		return data.get(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
 		//TODO: exercise 5b
-		return -1;
+		return 0;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		Contact contact = data.get(position);
 		LayoutInflater inflater = (LayoutInflater)context.getLayoutInflater();
-		View contactView = inflater.inflate(R.layout.contact_view, parent, false);
+		View contactView;
+		
+		if (position%2==0) {
+			contactView = inflater.inflate(R.layout.contact_view_even, parent, false);
+		} else {
+			contactView = inflater.inflate(R.layout.contact_view_odd, parent, false);
+		}
 		
 		// TODO: exercise 5
+		 ImageView contactPhotoImageView = (ImageView)contactView.findViewById(R.id.contact_photo_image_view);
+		    TextView contactNameTextView = (TextView)contactView.findViewById(R.id.contact_name_text_view);
+		    TextView contactTimesContactedTextView = (TextView)contactView.findViewById(R.id.contact_times_contacted_text_view);
+		    TextView contactLastTimeContactedTextView = (TextView)contactView.findViewById(R.id.contact_last_time_contacted_text_view);
+		    ImageView contactStarredImageView = (ImageView)contactView.findViewById(R.id.contact_starred_image_view);
+		 
+		    if (getCheckedItemPosition() == position) {
+		      contactView.setBackgroundColor(context.getResources().getColor(R.color.light_blue));
+		    } else {
+		      contactView.setBackgroundColor(context.getResources().getColor(R.color.light_gray));
+		    }
+		 
+		    AssetFileDescriptor assetFileDescriptor = null;
+		    try {
+		      Uri contactPhotoUri = contact.getPhoto();
+		      if (contactPhotoUri == Uri.EMPTY) {
+		        contactPhotoImageView.setImageResource(R.drawable.contact_photo);
+		      } else {
+		        assetFileDescriptor = context.getContentResolver().openAssetFileDescriptor(contactPhotoUri, "r");
+			FileDescriptor fileDescriptor = assetFileDescriptor.getFileDescriptor();
+			if (fileDescriptor != null) {
+			  contactPhotoImageView.setImageBitmap(BitmapFactory.decodeFileDescriptor(fileDescriptor, null, null));
+			}
+		      }
+		    } catch (FileNotFoundException fileNotFoundException) {
+		      Log.e(Constants.TAG, "An exception has occurred: "+fileNotFoundException.getMessage());
+		      if (Constants.DEBUG) {
+			fileNotFoundException.printStackTrace();
+		      }
+		    } finally {
+		      if (assetFileDescriptor != null) {
+		        try {
+		          assetFileDescriptor.close();
+			} catch (IOException ioException) {
+			  Log.e(Constants.TAG, "An exception has occurred: "+ioException.getMessage());
+		          if (Constants.DEBUG) {
+		            ioException.printStackTrace();
+		          }
+		        }
+		      }
+		    }
+		 
+		    contactNameTextView.setText(contact.getName());
+		    contactTimesContactedTextView.setText(context.getResources().getString(R.string.times_contacted)+" "+contact.getTimesContacted());
+		    long value = contact.getLastTimeContacted();
+		    if (value != 0) {
+		      contactLastTimeContactedTextView.setText(context.getResources().getString(R.string.last_time_contacted)+" "+Utilities.displayDateAndTime(value));
+		    } else {
+		      contactLastTimeContactedTextView.setText(context.getResources().getString(R.string.last_time_contacted)+" -");
+		    }
+		    if (contact.getStarred() == 1) {
+		      contactStarredImageView.setImageResource(R.drawable.star_set);
+		    } else {
+		      contactStarredImageView.setImageResource(R.drawable.star_unset);
+		    }
+		    return contactView;
 		
 		// TODO: exercise 7
 		
 		// TODO: exercise 8 (optional)
 		
-		return contactView;
+	}
+	
+	public final static int CONTACT_VIEW_TYPES     = 2;
+	
+	@Override
+	public int getViewTypeCount() {
+	  return CONTACT_VIEW_TYPES;
+	}
+	 
+	@Override
+	public int getItemViewType(int position) {
+	  return(position % CONTACT_VIEW_TYPES);
 	}
 
 	@Override
